7ffd04bca2c721269532d8e33e45d25edeb89733
hiyarhatiyar <GogolMogolkz@gmail.com>
2024-11-17 22:33:53 +0300

    выполнил модуль 2_2

---
+++ module_2_2_2.py
@@ -0,0 +1,29 @@
+# Все ли равны?
+# N1
+first = 1,2,3
+second = 4,5,6
+third = 7,8,9
+if first == second and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+# 2
+first = 42
+second = 69
+third = 42
+if first == second and second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
+
+# 3
+first = 1,2,3
+second = 4,5,6
+third = 7,8,9
+if not first == second and not second == third:
+    print(0)
